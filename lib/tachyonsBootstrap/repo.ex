d9181796bc31e2fb4f27891c340c04e7ca5899ec
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-22 16:16:38 +0100

    Initial Phoenix setup, #5

---
+++ lib/tachyonsBootstrap/repo.ex
@@ -0,0 +1,3 @@
+defmodule TachyonsBootstrap.Repo do
+  use Ecto.Repo, otp_app: :tachyonsBootstrap
+end
---
+++ test/views/page_view_test.exs
@@ -0,0 +1,3 @@
+defmodule TachyonsBootstrap.PageViewTest do
+  use TachyonsBootstrap.ConnCase, async: true
+end

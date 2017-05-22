13e4b615866f95f01075c143fe793490e51cc5e0
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-22 18:21:16 +0100

    Connects Heroku to the DB URL, #15

--- config/prod.exs
+++ config/prod.exs
@@ -20,6 +20,12 @@ config :tachyonsBootstrap, TachyonsBootstrap.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :tachyonsBootstrap, TachyonsBootstrap.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key

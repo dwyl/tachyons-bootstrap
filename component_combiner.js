7984dd0b0a92f8e14c534e8834c651f914f99181
katbow <katherine.bowler@gmail.com>
2017-05-31 16:43:24 +0100

    Load components from file #25 #26

--- component_combiner.js
+++ component_combiner.js
@@ -2,15 +2,28 @@ var http = require('http');
 var port = process.env.PORT || 8000;
 var fs = require('fs');
 
+var components = ['/components/typography.html', '/components/form.html', '/components/button.html', '/components/image.html'];
+
 var server = http.createServer(function (req, res) {
-  if (req.url.length === 1) {
+  if (req.url === '/') {
     homeHandler(req, res);
+  } else if (components.indexOf(req.url) > -1) {
+    componentHandler(req, res);
   }
 }).listen(port);
 
 function homeHandler (req, res) {
   res.writeHead(200, {'Content-type': 'text/html'});
   fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    res.end(data);
+  });
+}
+
+function componentHandler (req, res) {
+  var file = req.url.replace('/', '');
+  res.writeHead(200, {'Content-type': 'text/html'});
+  fs.readFile(file, function (err, data) {
     if (err) throw err;
     res.end(data);
   });
